557dca7fff67601f5104d02cb2025fbcaba3ff52
Christian Carig Clavecillas <clamumuu@gmail.com>
2022-06-03 11:15:33 +0800

    test for github

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.testForGitHub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testForGitHub
+            // 
+            this.testForGitHub.Location = new System.Drawing.Point(16, 14);
+            this.testForGitHub.Name = "testForGitHub";
+            this.testForGitHub.Size = new System.Drawing.Size(127, 82);
+            this.testForGitHub.TabIndex = 0;
+            this.testForGitHub.Text = "Test For Github";
+            this.testForGitHub.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testForGitHub);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button testForGitHub;
     }
 }
