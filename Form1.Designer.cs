69bb3238eea036b852ac762ef75dbb9ff4d6ebd1
Christian Carig Clavecillas <clamumuu@gmail.com>
2022-06-03 17:23:58 +0800

    improved and added bg pic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnR1C1 = new System.Windows.Forms.Button();
             this.btnR1C2 = new System.Windows.Forms.Button();
             this.btnR1C3 = new System.Windows.Forms.Button();
@@ -37,10 +38,13 @@
             this.btnR2C3 = new System.Windows.Forms.Button();
             this.btnR3C2 = new System.Windows.Forms.Button();
             this.btnR3C3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnR1C1
             // 
+            this.btnR1C1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR1C1.Location = new System.Drawing.Point(12, 12);
             this.btnR1C1.Name = "btnR1C1";
             this.btnR1C1.Size = new System.Drawing.Size(75, 75);
@@ -51,6 +55,7 @@
             // 
             // btnR1C2
             // 
+            this.btnR1C2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR1C2.Location = new System.Drawing.Point(93, 12);
             this.btnR1C2.Name = "btnR1C2";
             this.btnR1C2.Size = new System.Drawing.Size(75, 75);
@@ -61,6 +66,7 @@
             // 
             // btnR1C3
             // 
+            this.btnR1C3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR1C3.Location = new System.Drawing.Point(174, 12);
             this.btnR1C3.Name = "btnR1C3";
             this.btnR1C3.Size = new System.Drawing.Size(75, 75);
@@ -71,6 +77,7 @@
             // 
             // btnR2C1
             // 
+            this.btnR2C1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR2C1.Location = new System.Drawing.Point(12, 93);
             this.btnR2C1.Name = "btnR2C1";
             this.btnR2C1.Size = new System.Drawing.Size(75, 75);
@@ -81,6 +88,7 @@
             // 
             // btnR3C1
             // 
+            this.btnR3C1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR3C1.Location = new System.Drawing.Point(12, 174);
             this.btnR3C1.Name = "btnR3C1";
             this.btnR3C1.Size = new System.Drawing.Size(75, 75);
@@ -91,6 +99,7 @@
             // 
             // btnR2C2
             // 
+            this.btnR2C2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR2C2.Location = new System.Drawing.Point(93, 93);
             this.btnR2C2.Name = "btnR2C2";
             this.btnR2C2.Size = new System.Drawing.Size(75, 75);
@@ -100,6 +109,7 @@
             // 
             // btnR2C3
             // 
+            this.btnR2C3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR2C3.Location = new System.Drawing.Point(174, 93);
             this.btnR2C3.Name = "btnR2C3";
             this.btnR2C3.Size = new System.Drawing.Size(75, 75);
@@ -109,6 +119,7 @@
             // 
             // btnR3C2
             // 
+            this.btnR3C2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR3C2.Location = new System.Drawing.Point(93, 174);
             this.btnR3C2.Name = "btnR3C2";
             this.btnR3C2.Size = new System.Drawing.Size(75, 75);
@@ -118,6 +129,7 @@
             // 
             // btnR3C3
             // 
+            this.btnR3C3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnR3C3.Location = new System.Drawing.Point(174, 174);
             this.btnR3C3.Name = "btnR3C3";
             this.btnR3C3.Size = new System.Drawing.Size(75, 75);
@@ -125,11 +137,20 @@
             this.btnR3C3.UseVisualStyleBackColor = true;
             this.btnR3C3.Click += new System.EventHandler(this.btnR3C3_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-25, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(443, 267);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 273);
+            this.ClientSize = new System.Drawing.Size(403, 260);
             this.Controls.Add(this.btnR3C3);
             this.Controls.Add(this.btnR3C2);
             this.Controls.Add(this.btnR2C3);
@@ -139,8 +160,10 @@
             this.Controls.Add(this.btnR1C3);
             this.Controls.Add(this.btnR1C2);
             this.Controls.Add(this.btnR1C1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tic Tac Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -156,5 +179,6 @@
         private Button btnR2C3;
         private Button btnR3C2;
         private Button btnR3C3;
+        private PictureBox pictureBox1;
     }
 }
