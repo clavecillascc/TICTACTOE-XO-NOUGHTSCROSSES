6292b7a2520b693b31a152698305a4d180eebabc
Christian Carig Clavecillas <clamumuu@gmail.com>
2022-06-03 11:58:23 +0800

    click event for button row 1 column 1

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,17 @@ namespace TIC_TAC_TOE___A2___Clavecillas
 
         private void btnR1C1_Click(object sender, EventArgs e)
         {
-
+            if (click)
+            {
+                btnR1C1.Text = "X";
+                click = !click;
+            }
+            else
+            {
+                btnR1C1.Text = "Y";
+                click = !click;
+            }
+            btnR1C1.Enabled = false;
         }
     }
 }
