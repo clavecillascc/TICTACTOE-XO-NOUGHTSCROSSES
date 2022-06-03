b9f6e4c45ebf22da97c60ad23dbe8f0d53df4925
Christian Carig Clavecillas <clamumuu@gmail.com>
2022-06-03 11:13:59 +0800

    starting point

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace TIC_TAC_TOE___A2___Clavecillas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
